9c39e237f9e75e9e2622c31e6837855bb5c0ccd0
tempestaGui <guilhermeftempesta@gmail.com>
2026-02-02 20:16:25 -0300

    feat: add ImageImporter and Attributes class

---
+++ src/main/java/org/aplicacao/importers/ImageImporter.java
@@ -0,0 +1,28 @@
+package org.aplicacao.importers;
+
+import org.aplicacao.Importer;
+import org.aplicacao.entities.Document;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImageImporter implements Importer {
+
+    @Override
+    public Document importFIle(File file) throws IOException {
+        final Map<String, String> attributes = new HashMap<>();
+
+        attributes.put(PATH, file.getPath());
+
+        final BufferedImage image = ImageIO.read(file);
+        attributes.put(WIDTH, String.valueOf(image.getWidth()));
+        attributes.put(HEIGHT, String.valueOf(image.getHeight()));
+        attributes.put(TYPE, "Image");
+
+        return new Document(attributes);
+    }
+}
