5d20129ecb3711563f00c09936231cfbd9574594
tempestaGui <guilhermeftempesta@gmail.com>
2026-02-02 20:17:00 -0300

    feat: update DocumentManagementSystem

--- src/main/java/org/aplicacao/DocumentManagementSystem.java
+++ src/main/java/org/aplicacao/DocumentManagementSystem.java
@@ -1,9 +1,20 @@
 package org.aplicacao;
 
+import org.aplicacao.importers.ImageImporter;
+
 import javax.swing.text.Document;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DocumentManagementSystem {
+
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+
+    public DocumentManagementSystem(){
+        extensionToImporter.put("jpg", new ImageImporter());
+    }
+
     void importFile(String path){}
     List<Document> contents(){
         return null;
