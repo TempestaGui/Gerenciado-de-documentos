eb8c1a5e166c7bea61ca6e1c4ffc1b3fa6e6608f
tempestaGui <guilhermeftempesta@gmail.com>
2026-02-03 20:48:40 -0300

    feat: added functionality to the file import method

--- src/main/java/org/aplicacao/DocumentManagementSystem.java
+++ src/main/java/org/aplicacao/DocumentManagementSystem.java
@@ -1,8 +1,12 @@
 package org.aplicacao;
 
+import org.aplicacao.entities.Document;
 import org.aplicacao.importers.ImageImporter;
 
-import javax.swing.text.Document;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,13 +14,39 @@ import java.util.Map;
 public class DocumentManagementSystem {
 
     private final Map<String, Importer> extensionToImporter = new HashMap<>();
+    private final List<Document> documents = new ArrayList<>();
 
     public DocumentManagementSystem(){
         extensionToImporter.put("jpg", new ImageImporter());
     }
 
-    void importFile(String path){}
-    List<Document> contents(){
-        return null;
+   public void importFile(String path) throws IOException {
+        final File file = new File(path);
+
+        if(!file.exists()){
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf('.');
+        if(separatorIndex != -1){
+            if(separatorIndex == path.length()){
+                throw new UnknownError("No extensions found for file: "+path);
+            }
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+
+            if(importer == null){
+                throw new UnknownError("for file: "+path);
+            }
+
+            final Document document = importer.importFIle(file);
+            documents.add(document);
+        } else {
+            throw new UnknownError("no extendions found for file: "+path);
+        }
+    }
+
+    public List<Document> contents(){
+        return documents;
     }
 }
