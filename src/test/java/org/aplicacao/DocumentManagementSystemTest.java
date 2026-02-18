67673cbf631d94f6c386bf0f7979d5e06ead15f7
tempestaGui <guilhermeftempesta@gmail.com>
2026-02-18 20:12:29 -0300

    test: add import letter attributes test for DocumentManagementSystemTest

--- src/test/java/org/aplicacao/DocumentManagementSystemTest.java
+++ src/test/java/org/aplicacao/DocumentManagementSystemTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 
+import static org.aplicacao.Attributes.*;
+
 public class DocumentManagementSystemTest {
 
     private static final String RESOURCES = "src" + File.separator + "test" + File.separator + "resources" + File.separator;
@@ -19,5 +21,23 @@ public class DocumentManagementSystemTest {
         final Document document = OnlyDocument();
 
         assertAttributeEquals(document, Attributes.PATH, LETTER);
+    }
+
+    @Test
+    public void shouldImportLetterAttributes() throws Exception {
+        system.importFile(LETTER);
+
+        final Document document = onlyDocument();
+
+        assertAttributesEquals(document, PATIENT, JOE_BLOGGS);
+        assertAttributesEquals(document, ADDRESS,
+                    "123 Fake Street\n" +
+                     "Westminster\n" +
+                     "London\n" +
+                     "United Kingdom");
+        assertAttributeEquals(document, BODY,
+                "We are writing to you to confirm the re-scheduling of your appointment\n"+
+                "with Dr. Avaj from 29th December 2016 to 5th January 2017.");
+        assertTypeIs("LETTER", document);
     }
 }
